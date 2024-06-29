ee0db92650ffbcbd7a25612bd12bb7b95d7fbd4f
Hibari <jongxiankhoo@gmail.com>
2024-06-29 18:09:29 +0800

    feat: configure context for socket IO

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter } from 'react-router-dom'
-
-import { AuthProvider } from '@/hooks/AuthProvider'
-
+import { ChakraProvider } from '@chakra-ui/react'
 import { AppRoutes } from './routes'
+import { socket } from '@/store/socket'
+import { SocketContext } from '@/store/context'
+import { AuthProvider } from '@/hooks/useAuth'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <AppRoutes />
-      </AuthProvider>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <SocketContext.Provider value={socket}>
+            <AppRoutes />
+          </SocketContext.Provider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ChakraProvider>
   )
 }
