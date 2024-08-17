bcab3c4d80704f2ebe22529b5af5bad9e9085d77
Hibari <jongxiankhoo@gmail.com>
2024-08-17 21:19:20 +0800

    fix: fix screen viewport issue

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -50,7 +50,6 @@ export default function Chat() {
 
   const onGetUsers = async () => {
     const user = (await fetchUser()) || []
-    // console.log('Users:', user)
     setUsers(user)
     setLoading(false)
     return user
@@ -60,7 +59,7 @@ export default function Chat() {
     try {
       const res = await fetchChat(roomId)
       // console.log(res)
-      const sortedMessages = res?.sort((a: { createdAt: Date }, b: { createdAt: Date }) => {
+      const sortedMessages = await res?.sort((a: { createdAt: Date }, b: { createdAt: Date }) => {
         const left: Date | number = new Date(a?.createdAt)
         const right: Date | number = new Date(b?.createdAt)
         return Number(left) - Number(right)
@@ -162,17 +161,17 @@ export default function Chat() {
     const filteredUsers = users.filter((user) => user._id !== auth?.user._id) // remove ownself from list
 
     return (
-      <div className='-mx-2 mt-4 flex h-48 flex-col space-y-1 '>
+      <div className='flex flex-col space-y-1 -mx-2 mt-4 h-48'>
         {map(filteredUsers, (user, index) => (
           <button
             key={index}
             onClick={async () => onJoinChat(user?._id, user?.username)}
-            className='flex flex-row items-center rounded-xl p-2 hover:bg-gray-100 dark:hover:bg-gray-700'
+            className='flex flex-row items-center hover:bg-gray-100 dark:hover:bg-gray-700 p-2 rounded-xl'
           >
-            <div className='flex h-8 w-8 items-center justify-center rounded-full bg-indigo-200 dark:bg-indigo-600'>
+            <div className='flex justify-center items-center bg-indigo-200 dark:bg-indigo-600 rounded-full w-8 h-8'>
               {user?.username?.charAt(0)}
             </div>
-            <p className='ml-2 text-sm font-semibold dark:text-gray-200'>{user?.name}</p>
+            <p className='ml-2 font-semibold text-sm dark:text-gray-200'>{user?.name}</p>
           </button>
         ))}
       </div>
@@ -182,24 +181,24 @@ export default function Chat() {
   const renderMyMsg = (msg: string, sender_id?: string) => {
     // console.log('sender_id', sender_id)
     return sender_id === auth?.user?._id ? (
-      <div className='col-start-6 col-end-13 rounded-lg p-3'>
-        <div className='flex flex-row-reverse items-center justify-start'>
-          <div className='flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-indigo-500'>
+      <div className='col-start-6 col-end-13 p-3 rounded-lg'>
+        <div className='flex flex-row-reverse justify-start items-center'>
+          <div className='flex flex-shrink-0 justify-center items-center bg-indigo-500 rounded-full w-10 h-10'>
             Me
           </div>
-          <div className='relative mr-3 rounded-xl bg-indigo-100 px-4 py-2 text-sm shadow'>
+          <div className='relative bg-indigo-100 shadow mr-3 px-4 py-2 rounded-xl text-sm'>
             <p>{msg || `Sender`}</p>
           </div>
         </div>
       </div>
     ) : (
-      <div className='grid grid-cols-12 gap-y-2'>
-        <div className='col-start-1 col-end-8 rounded-lg p-3'>
+      <div className='gap-y-2 grid grid-cols-12'>
+        <div className='col-start-1 col-end-8 p-3 rounded-lg'>
           <div className='flex flex-row items-center'>
-            <div className='flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-indigo-500'>
+            <div className='flex flex-shrink-0 justify-center items-center bg-indigo-500 rounded-full w-10 h-10'>
               U
             </div>
-            <div className='relative ml-3 rounded-xl bg-white px-4 py-2 text-sm shadow'>
+            <div className='relative bg-white shadow ml-3 px-4 py-2 rounded-xl text-sm'>
               <p>{msg || `Recipient`}</p>
             </div>
           </div>
@@ -209,15 +208,15 @@ export default function Chat() {
   }
 
   return (
-    <div className=''>
+    <div className='h-screen max-h-[100dvh] overflow-x-hidden overflow-y-auto'>
       <CHeader />
-      <div className='h-screen max-h-lvh text-gray-800 antialiased'>
-        <div className='flex h-full w-full flex-row overflow-x-hidden'>
-          <div className='flex w-64 flex-shrink-0 flex-col bg-white py-4 pl-6 pr-2 dark:bg-gray-800'>
-            <div className='flex h-12 w-full flex-row items-center justify-center'>
-              <div className='flex h-10 w-10 items-center justify-center rounded-2xl bg-indigo-100 text-indigo-700 dark:bg-indigo-700 dark:text-indigo-100'>
+      <div className='flex flex-col max-w-full h-full max-h-[100dvh] text-gray-800 antialiased overflow-x-hidden'>
+        <div className='flex flex-row flex-grow h-full overflow-x-hidden'>
+          <div className='flex flex-col flex-shrink-0 bg-white dark:bg-gray-800 py-4 pr-2 pl-6 w-64'>
+            <div className='flex flex-row justify-center items-center w-full h-12'>
+              <div className='flex justify-center items-center bg-indigo-100 dark:bg-indigo-700 rounded-2xl w-10 h-10 text-indigo-700 dark:text-indigo-100'>
                 <svg
-                  className='h-6 w-6'
+                  className='w-6 h-6'
                   fill='none'
                   stroke='currentColor'
                   viewBox='0 0 24 24'
@@ -231,33 +230,33 @@ export default function Chat() {
                   ></path>
                 </svg>
               </div>
-              <div className='ml-2 text-2xl font-bold dark:text-gray-200'>QuickChat</div>
+              <div className='ml-2 font-bold text-2xl dark:text-gray-200'>QuickChat</div>
             </div>
-            <div className='mt-8 flex flex-col'>
-              <div className='flex flex-row items-center justify-between text-xs'>
+            <div className='flex flex-col mt-8'>
+              <div className='flex flex-row justify-between items-center text-xs'>
                 <span className='font-bold dark:text-gray-200'>Active Conversations</span>
-                <span className='flex h-4 w-4 items-center justify-center rounded-full bg-gray-300 dark:bg-gray-600'>
-                  4
+                <span className='flex justify-center items-center bg-gray-300 dark:bg-gray-600 rounded-full w-4 h-4'>
+                  {users?.length - 1 || 0}
                 </span>
               </div>
               {renderUsers()}
             </div>
           </div>
-          <div className='flex h-full max-h-screen flex-auto flex-col p-4'>
-            <div className='flex h-full flex-auto flex-shrink-0 flex-col rounded-2xl bg-gray-100 p-4 dark:bg-gray-700'>
-              <div className='mb-4 flex flex-col  overflow-x-auto'>
-                <div className='flex flex-col '>
+          <div className='flex flex-col flex-grow flex-auto my-2 px-4 max-h-[calc(100dvh-32px)]'>
+            <div className='flex flex-col bg-gray-100 dark:bg-gray-700 p-2 rounded-2xl h-full overflow-y-auto'>
+              <div className='flex-col whitespace-nowrap overflow-y-auto no-scrollbar'>
+                <div className='flex-col'>
                   {messages.map((data: any, index) => (
                     <div key={index}>{renderMyMsg(data?.content, data?.sender_id?._id)}</div>
                   ))}
                 </div>
               </div>
             </div>
-            <div className='flex h-16 w-full flex-row items-center rounded-xl bg-white px-4 dark:bg-gray-800'>
-              <div>
-                <button className='flex items-center justify-center text-gray-400 hover:text-gray-600 dark:text-gray-500 dark:hover:text-gray-400'>
+            <div className='flex flex-row items-center bg-white dark:bg-gray-800 mt-1 px-4 rounded-xl w-full h-16'>
+              <div className='flex'>
+                <button className='flex justify-center items-center text-gray-400 hover:text-gray-600 dark:hover:text-gray-400 dark:text-gray-500'>
                   <svg
-                    className='h-5 w-5'
+                    className='w-5 h-5'
                     fill='none'
                     stroke='currentColor'
                     viewBox='0 0 24 24'
@@ -272,18 +271,18 @@ export default function Chat() {
                   </svg>
                 </button>
               </div>
-              <div className='ml-4 flex-grow'>
+              <div className='flex-grow ml-4'>
                 <div className='relative w-full'>
                   <Input
                     type='text'
                     value={messageInput?.content}
                     placeholder='type something...'
                     onChange={(e) => onSetMsgInput(e?.target?.value)}
-                    className='flex h-10 w-full rounded-xl border pl-4 focus:border-indigo-300 focus:outline-none dark:border-gray-600 dark:focus:border-indigo-500'
+                    className='flex focus:border-indigo-300 dark:focus:border-indigo-500 dark:border-gray-600 pl-4 border rounded-xl w-full h-10 focus:outline-none'
                   />
-                  <button className='absolute right-0 top-0 flex h-full w-12 items-center justify-center text-gray-400 hover:text-gray-600 dark:text-gray-500 dark:hover:text-gray-400'>
+                  <button className='top-0 right-0 absolute flex justify-center items-center w-12 h-full text-gray-400 hover:text-gray-600 dark:hover:text-gray-400 dark:text-gray-500'>
                     <svg
-                      className='h-6 w-6'
+                      className='w-6 h-6'
                       fill='none'
                       stroke='currentColor'
                       viewBox='0 0 24 24'
@@ -302,12 +301,12 @@ export default function Chat() {
               <div className='ml-4'>
                 <Button
                   onPress={sendMessage}
-                  className='flex flex-shrink-0 items-center justify-center rounded-xl bg-indigo-500 px-4 py-1 text-white hover:bg-indigo-600 dark:bg-indigo-600 dark:hover:bg-indigo-700'
+                  className='flex flex-shrink-0 justify-center items-center bg-indigo-500 hover:bg-indigo-600 dark:hover:bg-indigo-700 dark:bg-indigo-600 px-4 py-1 rounded-xl text-white'
                 >
                   <span>Send</span>
                   <span className='ml-2'>
                     <svg
-                      className='-mt-px h-4 w-4 rotate-45 transform'
+                      className='-mt-px w-4 h-4 transform rotate-45'
                       fill='none'
                       stroke='currentColor'
                       viewBox='0 0 24 24'
